c0affea75567928f032b140ab3e386c7bb377a97
aolofsson <andreas@zeroasic.com>
2023-10-01 09:06:26 -0400

    Fixing script for separate header file

--- update.py
+++ update.py
@@ -13,28 +13,39 @@ def main():
     alumni = list(csv.DictReader(open("alumni.csv", "r"), delimiter=","))
 
     # Read in README
-    with open('README.md') as f:
-        readme = f.read().splitlines()
+    with open('header.md') as f:
+        header = f.read().splitlines()
 
     # Insert csv lists
     with open('README.md', 'w') as f:
-        for line in readme:
-            print(line, file=f)
-            if re.search(r'\(startups.csv\)', line):
-                print("\n| Company | Technology | Founded | Country | Description |", file=f)
-                print("|---------|------------|---------|---------|-------------|", file=f)
-                for x in startups:
-                    print(f"|[{x['Company']}](https://{x['Website']}) | {x['Technology']} | {x['Country']} | {x['Founded']} |{x['Description']} |", file=f)
-            elif re.search(r'\(alumni.csv\)', line):
-                print("\n| Company |  Exit   | Year   | Value | Link |", file=f)
-                print("|---------| ------- | ------ | ------|------|", file=f)
-                for x in alumni:
-                    if not re.search(r'http', x['Link']):
-                        link = "NA"
-                    else:
-                        link = f"[Source]({x['Link']})"
-                    print(f"|{x['Company']} | {x['Exit']} | {x['Year']} | {x['Value']} | {link} |", file=f)
 
+        ################################
+        # Printing out old README header
+        ################################
+        for line in header:
+            print(line, file=f)
+
+        ################################
+        # Printing out all startups
+        ################################
+        print("\n## Startups", file=f)
+        print("\n| Company | Technology | Founded | Country | Description |", file=f)
+        print("|---------|------------|---------|---------|-------------|", file=f)
+        for x in startups:
+            print(f"|[{x['Company']}](https://{x['Website']}) | {x['Technology']} | {x['Country']} | {x['Founded']} |{x['Description']} |", file=f)
+
+        ################################
+        # Printing out exits
+        ################################
+        print("\n## Alumni", file=f)
+        print("\n| Company |  Exit   | Year   | Value | Link |", file=f)
+        print("|---------| ------- | ------ | ------|------|", file=f)
+        for x in alumni:
+            if not re.search(r'http', x['Link']):
+                link = "NA"
+            else:
+                link = f"[Source]({x['Link']})"
+                print(f"|{x['Company']} | {x['Exit']} | {x['Year']} | {x['Value']} | {link} |", file=f)
 
 if __name__ == '__main__':
     main()
